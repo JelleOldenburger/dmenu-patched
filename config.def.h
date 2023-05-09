51741c97684ffcd4d2624157eb1d19127bd4f6c6
Jelle Oldenburger <j.r.oldenburger@gmail.com>
2023-05-09 18:08:53 +0200

    Modified dmenu colors

--- config.def.h
+++ config.def.h
@@ -11,10 +11,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#bbbbbb", "#2F343F" },
+	[SchemeSel] = { "#eeeeee", "#990000" },
+	[SchemeSelHighlight] = { "#ECA72D", "#990000" },
+	[SchemeNormHighlight] = { "#ECA72D", "#2F343F" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
